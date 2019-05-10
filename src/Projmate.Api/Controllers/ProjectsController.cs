0d9d9bdb45a0f7661ff100b0b403cdcd5c3c1b46
Steve <Steve.mccann@live.com>
2019-05-10 12:08:26 -0400

    Added Root and ProjectsController.cs

---
+++ src/Projmate.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projmate.Api.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet(Name =nameof(GetProjects))]
+        public IActionResult GetProjects()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Projmate.Api/Controllers/RootController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projmate.Api.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name =nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                Projects = new
+                {
+                    href = Url.Link(nameof(ProjectsController.GetProjects),null)
+                }
+            };
+            return Ok(response);
+        }
+    }
+}
